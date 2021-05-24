301070e3e1c50d6856d2f84b560e12b807733bee
WilBerlin <wiwijaja@googlemail.com>
2021-05-24 13:44:06 +0200

    Add Enemy

--- main.ts
+++ main.ts
@@ -12,24 +12,42 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     Shoot = game.createSprite(Player.get(LedSpriteProperty.X), Player.get(LedSpriteProperty.Y))
-    Shoot.change(LedSpriteProperty.Brightness, 100)
+    Shoot.change(LedSpriteProperty.Brightness, 255)
     for (let index = 0; index < 4; index++) {
         Shoot.change(LedSpriteProperty.Y, -1)
         basic.pause(100)
     }
     Shoot.delete()
 })
+let Enemy: game.LedSprite = null
 let Shoot: game.LedSprite = null
 let Player: game.LedSprite = null
 let GameStarted = 0
 let dirPlayer = 0
 dirPlayer = 1
 GameStarted = 0
+let Enemies = 0
 basic.forever(function () {
     if (GameStarted == 1) {
         Player.move(dirPlayer)
         basic.pause(120)
     } else {
         basic.showArrow(ArrowNames.West)
+    }
+})
+basic.forever(function () {
+    if (GameStarted == 1 && Enemies == 0) {
+        Enemies = 1
+        Enemy = game.createSprite(randint(0, 4), 0)
+        Enemy.set(LedSpriteProperty.Brightness, 80)
+        for (let index = 0; index < 4; index++) {
+            Enemy.change(LedSpriteProperty.Y, 1)
+            basic.pause(200)
+        }
+        if (Enemy.isTouching(Player)) {
+            game.gameOver()
+        }
+        Enemy.delete()
+        Enemies = 0
     }
 })
